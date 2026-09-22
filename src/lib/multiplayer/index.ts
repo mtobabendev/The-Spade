a6004d5dd4e62b55ed46a21733c0b2d09b4eb989
Matt Tobaben <mtobabendev@gmail.com>
2026-09-22 06:11:56 -0500

    Remove obsolete AV mesh export

--- src/lib/multiplayer/index.ts
+++ src/lib/multiplayer/index.ts
@@ -1,4 +1,3 @@
 export { P2PRoom, defaultIceServers } from "./p2p";
 export type { PeerInfo, PeerRow, RtcPollResponse, SignalKind, SignalRow } from "./p2p";
 export { useP2PRoom } from "./use-p2p-room";
-export { useAvTable } from "./use-av-table";
